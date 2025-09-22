fe33e6ab8bd4857db3fe5165ac2e3b32181f76b6
SeyoungCho <wnl383@naver.com>
2025-09-23 08:53:26 +0900

    feat: add number-ticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import HeroSection from "@/sections/hero";
 import Navbar from "@/components/common/navbar";
-import IntroSection from "@/sections/intro";
-import RoutineSection from "@/sections/routine";
-import ContactSection from "@/sections/contact";
+// import IntroSection from "@/sections/intro";
+// import RoutineSection from "@/sections/routine";
+// import ContactSection from "@/sections/contact";
 
 export default function Home() {
   return (
@@ -10,11 +10,11 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <div className="dummy h-[clamp(64rem,51.259rem+56.6265vw,87.5rem)] lg:h-screen" />
-      <IntroSection />
+      {/* <IntroSection />
       <RoutineSection />
       <div className="bg-neutral-50/40">
         <ContactSection />
-      </div>
+      </div> */}
     </main>
   );
 }
